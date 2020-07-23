384f7065bc154e93f65e2c1a8fb2b73d8bf9228d
MeitarFuchs <meitardar2@gmail.com>
2020-07-23 11:16:09 +0300

    start again EX5

---
+++ cmake-build-debug/CMakeFiles/Itertools.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Itertools.dir/main.cpp.o"
+  "Itertools"
+  "Itertools.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Itertools.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
